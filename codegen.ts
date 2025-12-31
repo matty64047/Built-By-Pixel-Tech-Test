3a48ba3bdd793bbd4217922cc589772a0a9dad50
Matt Connolly <54942633+matty64047@users.noreply.github.com>
2025-12-31 16:18:57 +0000

    changes

--- codegen.ts
+++ codegen.ts
@@ -1,25 +1,25 @@
-import 'dotenv/config';
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import "dotenv/config";
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  overwrite: true,
+	overwrite: true,
 
-  schema: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+	schema: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
 
-  documents: 'graphql/**/*.ts',
+	documents: "graphql/**/*.ts",
 
-  generates: {
-    './graphql/generated.ts': {
-      plugins: [
-        'typescript',
-        'typescript-operations',
-        'typescript-react-apollo',
-      ],
-      config: {
-        withHooks: false,
-      },
-    },
-  },
+	generates: {
+		"./graphql/generated.ts": {
+			plugins: [
+				"typescript",
+				"typescript-operations",
+				"typescript-react-apollo",
+			],
+			config: {
+				withHooks: false,
+			},
+		},
+	},
 };
 
 export default config;
